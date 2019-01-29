13eb0847b6e9a459b026a23fa93b24357313e20d
Samuel Adewole <samueladewole15@gmail.com>
2019-01-29 21:49:58 +0100

    initialized UI

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const time = 0;
+const count = 0;
+
+
+
+// general variables;
+const randMasg = document.querySelector('#inp');
+const inp = document.querySelector('#inp');
+const btn = document.querySelector('#btn')
+const message = document.querySelector('#message')
+const timer = document.querySelector('#timer')
+const score = document.querySelector('#score');
+
+
