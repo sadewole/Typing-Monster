bee3aaf65c945c7bc99572c1c5eb28e5d942a8ac
Samuel Adewole <samueladewole15@gmail.com>
2019-01-30 00:26:46 +0100

    worked on main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-const time = 0;
-const count = 0;
+let time=6;
+let count = 0;
 
 
 
@@ -12,3 +12,71 @@ const timer = document.querySelector('#timer')
 const score = document.querySelector('#score');
 
 
+let arr = ['Bada', 'Tunji', 'Peter', 'Pokeman', 'Step']
+
+// Generate random number
+function genRand(val){
+    let word = Math.floor(Math.random() * val.length);
+    return randMsg.innerHTML = val[word];
+}
+
+// Check for match
+function wordMatch(){
+    if(inp.value===randMsg.innerHTML){
+        message.innerHTML= 'Correct!!!';
+        return true;
+    }else{
+        message.innerHTML = '';
+        return false;
+    }
+}
+
+// get match and process
+function catchMatch(){
+    if(wordMatch()){
+        inp.value='';
+        time=6
+        count++
+        genRand(arr)
+    }
+    score.innerHTML = count;
+}
+
+// Set Timer
+function setCount(){
+    if(time>0){
+        time--
+    }else if(time===0){
+        btn.removeAttribute('disabled')
+        randMsg.style.display = 'none';
+        message.innerHTML = 'GameOver!!!'
+    }
+    timer.innerHTML= time
+}
+
+// Reset function
+function reset(){
+    if(!btn.hasAttribute('disabled') && message.innerHTML == 'GameOver!!!'){
+        console.log('coment')
+        randMsg.innerHTML = ' ';
+        clearInterval(setInterval(setCount, 1000))
+    }
+}
+
+start=(e)=>{
+    clearInterval(setInterval(setCount, 1000))
+    time=6
+    const btn = e.target;
+    btn.setAttribute('disabled', 'disabled');
+    //reset()
+    randMsg.style.display='block'
+    
+     genRand(arr)
+     setInterval(setCount, 1000)
+     inp.addEventListener('input', catchMatch)
+}
+
+startGame=()=>{
+    btn.addEventListener('click', start)
+}
+startGame();
